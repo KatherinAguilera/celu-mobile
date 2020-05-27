10a3d7423c5032774d7ce3d6612c0f6d37dade44
Katherine Aguilera <katherine.aguilera@huelladigital.tv>
2020-05-27 16:01:53 -0500

    Ajustes estilos mobile

---
+++ build/precache-manifest.d27938f3849fe06fc27890a5f1387788.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de07dd33eaa5c41e90f58052317a7393",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a2f74c40ce0149137a03",
+    "url": "/static/css/main.0c4a6a00.chunk.css"
+  },
+  {
+    "revision": "52bd1c33a2499ef2f22b",
+    "url": "/static/js/2.fb8fc360.chunk.js"
+  },
+  {
+    "revision": "a2f74c40ce0149137a03",
+    "url": "/static/js/main.c9c17015.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "0f6e082f42f5247d82c3ade9c2a8f928",
+    "url": "/static/media/Gilroy-ExtraBold.0f6e082f.otf"
+  },
+  {
+    "revision": "d4711be125163b4343f71c6e5796a099",
+    "url": "/static/media/mobile.d4711be1.png"
+  }
+]);
